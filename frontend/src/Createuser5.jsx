8dfce2f215cb2f9641fb4015a0d218cd9606fcf2
siva-mani-r3 <99952847+siva-mani-r3@users.noreply.github.com>
2024-12-27 19:45:34 +0530

    Update Createuser5.jsx

--- frontend/src/Createuser5.jsx
+++ frontend/src/Createuser5.jsx
@@ -7,7 +7,7 @@ const Createuser5 = () => {
     const navigate = useNavigate()
     const Submit=(e)=>{
         e.preventDefault();
-        axios.post('http://localhost:3002/create5',{name,rollno})
+        axios.post('https://hms-backend-zq7j.onrender.com/create5',{name,rollno})
         .then(result=>{console.log(result)
             navigate('/users5')
         })
