03fe224e5af48ea5de2be31e024fc75a021b81aa
siva-mani-r3 <99952847+siva-mani-r3@users.noreply.github.com>
2024-12-29 08:22:01 +0530

    Update Createuser3.jsx

--- frontend/src/Createuser3.jsx
+++ frontend/src/Createuser3.jsx
@@ -7,7 +7,7 @@ const Createuser3 = () => {
     const navigate = useNavigate()
     const Submit=(e)=>{
         e.preventDefault();
-        axios.post('https://hms-backend-zq7j.onrender.com/create3',{name,rollno})
+        axios.post('https://hms-api-six.vercel.app/create3',{name,rollno})
         .then(result=>{console.log(result)
             navigate('/users3')
         })
