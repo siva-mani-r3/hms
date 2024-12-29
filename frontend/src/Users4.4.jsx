eab6ed22780b46553516b87ac871cc97d5516f76
siva-mani-r3 <99952847+siva-mani-r3@users.noreply.github.com>
2024-12-29 08:30:38 +0530

    Update Users4.4.jsx

--- frontend/src/Users4.4.jsx
+++ frontend/src/Users4.4.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 function Users44 ()  {
 const[users,setusers]=useState([]) 
 useEffect(()=>{
-    axios.get('https://hms-backend-zq7j.onrender.com/users4')
+    axios.get('https://hms-api-six.vercel.app/users4')
     .then(result=>setusers(result.data))
     .catch(err=>console.log(err))
 },[])
