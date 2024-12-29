848ef52033a1eb733a4a9bb78116b3d5bd26adab
siva-mani-r3 <99952847+siva-mani-r3@users.noreply.github.com>
2024-12-29 08:32:39 +0530

    Update Users7.7.jsx

--- frontend/src/Users7.7.jsx
+++ frontend/src/Users7.7.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 function Users77 ()  {
 const[users,setusers]=useState([]) 
 useEffect(()=>{
-    axios.get('https://hms-backend-zq7j.onrender.com/users7')
+    axios.get('https://hms-api-six.vercel.app/users7')
     .then(result=>setusers(result.data))
     .catch(err=>console.log(err))
 },[])
