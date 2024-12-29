57629ec7f8edde5ce67e8fd78d9bd07f4b702f31
siva-mani-r3 <99952847+siva-mani-r3@users.noreply.github.com>
2024-12-29 08:32:25 +0530

    Update Users6.jsx

--- frontend/src/Users6.jsx
+++ frontend/src/Users6.jsx
@@ -4,14 +4,14 @@ import axios from 'axios'
 function Users6 ()  {
 const[users6,setusers6]=useState([]) 
 useEffect(()=>{
-    axios.get('https://hms-backend-zq7j.onrender.com/users6')
+    axios.get('https://hms-api-six.vercel.app/users6')
     .then(result=>setusers6(result.data))
     .catch(err=>console.log(err))
 
 
 },[])
 const handleDelete =(id)=>{
-    axios.delete('https://hms-backend-zq7j.onrender.com/deleteUser6/'+id)
+    axios.delete('https://hms-api-six.vercel.app/deleteUser6/'+id)
     .then(res=>{console.log(res)
 window.location.reload()})
     .catch(err=>console.log(err))
